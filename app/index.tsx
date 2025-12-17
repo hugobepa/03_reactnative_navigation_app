d53720888d9220a74c982d1f584fc81b4faef510
hugobepa <hugo_bcn@hotmail.com>
2025-12-17 19:13:25 +0100

    appNavigation_CustomColorTailwind

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const App  = () => {
   return (
 
-    <SafeAreaView>
-             <View className='mt-5 mx-2.5'>
+    <SafeAreaView >
+             <View className='mt-5 mx-2.5 bg-primary '>
               <Text 
               style={{fontFamily: 'WorkSans-Black'}}
               className='text-5xl '>Hola Mundo</Text>
@@ -14,6 +14,10 @@ const App  = () => {
 
               <Text className='text-4xl font-work-medium'>Hola Mundo</Text>
             <Text className='text-3xl font-work-light'>Hola Mundo</Text>
+            <Text className='text-4xl font-work-medium text-primary'>Hola Mundo</Text>
+            <Text className='text-3xl font-work-light text-secondary'>Hola Mundo</Text>
+            <Text className='text-3xl font-work-light text-secondary-100'>Hola Mundo</Text>
+            <Text className='text-3xl font-work-light text-tertiary'>Hola Mundo</Text>
              </View>
     </SafeAreaView>
 
