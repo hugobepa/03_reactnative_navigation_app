86fbb8225d4d673a267af17133de980a275141f5
hugobepa <hugo_bcn@hotmail.com>
2025-12-18 19:32:33 +0100

    appTabsDrawer_customDrawer

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,14 +1,29 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
 import React from 'react';
 
 const DrawerLayout = () => {
   return (
-   <Drawer>
+   <Drawer
+    screenOptions={{
+        //overlayColor: 'red',
+        overlayColor: 'rgba(0,0,0,0.4)',
+        drawerActiveTintColor: 'indigo', 
+        headerShadowVisible: false,
+         sceneStyle: {
+          backgroundColor: 'white',
+        },
+    }}
+   
+   >
       <Drawer.Screen
         name="user/index" // This is the name of the page and must match the url from root
          options={{
           drawerLabel: 'User',
           title: 'Usuario',
+          drawerIcon:({color,size})=>(
+            <Ionicons name='person-circle-outline' color={color} size={size}  />
+          )
         }}
       />
       <Drawer.Screen
@@ -16,6 +31,10 @@ const DrawerLayout = () => {
         options={{
           drawerLabel: 'Horario',
           title: 'Horario',
+          drawerIcon:({color,size})=>(
+            <Ionicons name='calendar-outline' color={color} size={size}  />
+          )
+          
         }}
       />
     </Drawer>
